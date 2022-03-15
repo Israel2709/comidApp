3278352ab08504c7c1ae5488234ea7dceaf2b84a
Israel Salinas <israel.salinas.m@gmail.com>
2022-03-14 20:28:00 -0600

    fetching-data, useEffect

--- src/App.js
+++ src/App.js
@@ -1,124 +1,31 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import './App.scss'
-import Balloon from './Components/Balloon'
 
 function App () {
-  const [balloonsRequirement, setBalloonsRequirement] = useState([
-    {
-      shape: 'square',
-      color: 'peru',
-      size: 'small'
-    },
-    {
-      shape: 'redondo',
-      color: 'red',
-      size: 'big'
-    },
-    {
-      shape: 'square',
-      color: 'purple',
-      size: 'big'
-    },
-    {
-      shape: 'long',
-      color: 'slategray',
-      size: 'normal'
-    }
-  ])
-  const [balloonObject, setBalloonObject] = useState({})
-  const [cardNumber, setCardNumber] = useState('')
-
-  const balloonInputHandler = event => {
-    let property = event.target.name
-    let value = event.target.value
-    setBalloonObject({ ...balloonObject, [property]: value })
-  }
-
-  const addBalloon = () => {
-    setBalloonsRequirement([...balloonsRequirement, balloonObject])
-  }
-
-  const getCardNumber = event => {
-    setCardNumber(event.target.value)
-  }
-  const deleteBalloon = event => {
-    console.log(event.target.dataset.balloonIndex)
-    let balloonIndex = event.target.dataset.balloonIndex
-    let ballonsList = balloonsRequirement
-    ballonsList.splice(balloonIndex, 1)
-    setBalloonsRequirement([...ballonsList])
-  }
+  const [dishes, setDishes] = useState({})
+  const [reFetch, setReFetch] = useState(false)
+  useEffect(() => {
+    console.log('componente renderizado')
+    let data = fetch(
+      'https://react-crud-15g-default-rtdb.firebaseio.com/dishes.json'
+    ).then(response => {
+      response.json().then(json => {
+        setDishes(json)
+      })
+    })
+  }, [])
   return (
     <div className='App'>
-      <div className='container-fluid'>
-        <div className='row'>
-          <div className='col-12 col-md-8'>
-            <div className='d-flex'>
-              {balloonsRequirement.map((balloon, index) => {
-                const { shape, color, size } = balloon
-                return (
-                  <Balloon
-                    key={index}
-                    shape={shape}
-                    color={color}
-                    size={size}
-                    deleteHandler={deleteBalloon}
-                    balloonKey={index}
-                  />
-                )
-              })}
-            </div>
+      {Object.keys(dishes).map(dish => {
+        const { name, region, type } = dishes[dish]
+        return (
+          <div className='card p-3 border rounded'>
+            <h2>{name}</h2>
+            <h3>{region}</h3>
+            <h4>{type}</h4>
           </div>
-          <div className='col-12 col-md-4'>
-            <form action='' className='bg-dark text-white border rounded p-3'>
-              <div className='form-group mb-3'>
-                <label htmlFor=''>Tamaño</label>
-                <select
-                  name='size'
-                  id=''
-                  className='form-select'
-                  aria-label='Default select example'
-                  onChange={balloonInputHandler}
-                >
-                  <option value='small'>Pequeño</option>
-                  <option value='normal'>Normal</option>
-                  <option value='big'>Grande</option>
-                </select>
-              </div>
-              <div className='form-group mb-3'>
-                <label htmlFor=''>Color</label>
-                <input
-                  type='color'
-                  name='color'
-                  className='form-control'
-                  onChange={balloonInputHandler}
-                />
-              </div>
-              <div className='form-group mb-3'>
-                <label htmlFor=''>Forma</label>
-                <select
-                  name='shape'
-                  id=''
-                  className='form-select'
-                  aria-label='Default select example'
-                  onChange={balloonInputHandler}
-                >
-                  <option value='square'>Cuadrado</option>
-                  <option value='redondo'>Redondo</option>
-                  <option value='long'>Largo</option>
-                </select>
-              </div>
-              <button
-                type='button'
-                className='btn btn-outline-light'
-                onClick={addBalloon}
-              >
-                Agregar Globo
-              </button>
-            </form>
-          </div>
-        </div>
-      </div>
+        )
+      })}
     </div>
   )
 }
